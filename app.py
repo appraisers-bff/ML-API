5a0b5f0f8ee1904247aa1a583f16144f08e3474a
peytonrunyan <44583861+peytonrunyan@users.noreply.github.com>
2019-04-14 15:11:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import _pickle as pickle
 from pandas.io.json import json_normalize
 from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults
 
-reg = pickle.load(open('reg_model.pkl','rb'))
+# pickled multiple regression model
+model = pickle.load(open('reg_model.pkl','rb'))
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['POST'])
+# read in data 
 def make_predict():
     lines = request.get_json(force=True)
     address = lines['address']
@@ -20,10 +22,12 @@ def make_predict():
     SqFt = lines['SqFt']
     year = lines['year'] 
     
+    # generate prediction using pickled model
     input = np.array([[bath, bed, SqFt, year, zipcode]])
-    prediction = reg.predict(input)
+    prediction = model.predict(input)
     output = list(prediction[0])[0]
     
+    # compare against zillow model
     zillow_data = ZillowWrapper(API_KEY_HERE)
     deep_search_response = zillow_data.get_deep_search_results(address,zipcode)
     result = GetDeepSearchResults(deep_search_response)
